ee5e50522330581d2500d47c5548951f547c9b60
raymondborel <rborel852@gmail.com>
2023-07-14 13:49:40 -0700

    created post list route

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
+    path('posts/', views.PostList.as_view(), name="artist_list"),
     # path('users/', views.UserList.as_view(), name="user_list"),
 ]
