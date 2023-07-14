42cfcc98dd249deb30064a32ce45c8256cc44bc7
raymondborel <rborel852@gmail.com>
2023-07-13 22:42:43 -0700

    created home

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-# this like app.use() in express
 urlpatterns = [
-
+    path('', views.Home.as_view(), name="home"),
+    path('users/', views.UserList.as_view(), name="user_list"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views import View
 
 # Create your views here.
+class Home(TemplateView):
+    template_name = "home.html"
+    
