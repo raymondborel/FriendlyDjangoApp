bec18f372f381f7d3ed8042a1d1a1f6ed5fd7691
raymondborel <rborel852@gmail.com>
2023-07-15 23:58:10 -0700

    created post create route

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('posts/', views.PostList.as_view(), name="post_list"),
+    path('posts/new/', views.PostCreate.as_view(), name="post_create")
 ]
