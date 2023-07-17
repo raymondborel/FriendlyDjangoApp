08d409af504bca9ea509ff07f308862436d8fb6a
raymondborel <rborel852@gmail.com>
2023-07-17 15:08:43 -0700

    Added User model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 # from django.contrib.auth.models import User
 
 # Create your models here.
@@ -8,7 +9,7 @@ class Post(models.Model):
     img = models.CharField(max_length=250)
     body = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     
     def __str__(self):
         return self.title
