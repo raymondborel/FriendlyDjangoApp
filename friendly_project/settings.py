ffd247333ba940432714eddf286a53dea1bc65ae
raymondborel <rborel852@gmail.com>
2023-07-18 13:08:09 -0700

    Added catagory model

--- friendly_project/settings.py
+++ friendly_project/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
 LOGIN_REDIRECT_URL = '/posts/'
 LOGOUT_REDIRECT_URL = '/'
 
+
 ROOT_URLCONF = 'friendly_project.urls'
 
 TEMPLATES = [
--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django.contrib.auth.models import User
 
-# Create your models here.
+class Catagory(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
 
     title = models.CharField(max_length=100)
@@ -10,6 +13,7 @@ class Post(models.Model):
     body = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    catagory = models.CharField(max_length=200, default='opentoanything')
     
     def __str__(self):
         return self.title
