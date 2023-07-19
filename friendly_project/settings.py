eadefb954c829d217bc29413e8fc5fe4c3628127
raymondborel <rborel852@gmail.com>
2023-07-19 15:44:56 -0700

    deploying

--- friendly_project/settings.py
+++ friendly_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "53245h4534k")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
