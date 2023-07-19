2925ea902ebe00777eaaa55d357869fe2bfeeca2
raymondborel <rborel852@gmail.com>
2023-07-18 21:38:35 -0700

    trying to deploy

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
+#!/usr/bin/env bash
+
+set -o errexit  # exit on error
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
 # Install Dependencies
-pip3 install -r deps.txt
+# pip3 install -r deps.txt
 
 # Run Migration
-python3 manage.py migrate
+# python3 manage.py migrate
