f4b600471937cf7a901e255bad6d9029dae41041
raymondborel <rborel852@gmail.com>
2023-07-18 21:46:08 -0700

    still trouble shooting

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-set -o errexit  # exit on error
+# set -o errexit  # exit on error
 
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+# python manage.py collectstatic --no-input
+# python manage.py migrate
 
+
+# video method 
 # Install Dependencies
-# pip3 install -r deps.txt
+pip3 install -r deps.txt
 
 # Run Migration
-# python3 manage.py migrate
+python3 manage.py migrate
